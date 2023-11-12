66dd5e1bab32e7c9fd46a4015ccdceaf60a17846
Gabriel <gbastosrabelo@gmail.com>
2023-11-11 21:31:20 -0300

    updating new questions

--- lista de exercícios para a 2 prova/linkedList.c
+++ lista de exercícios para a 2 prova/linkedList.c
@@ -181,3 +181,160 @@ int cdllInsertAsFirst(CDLList *l, void *data){
     return FALSE;
 }
 
+//questão 1 letra b
+
+//para lista simplesmente encadeada
+int sllInsertAsLast(SLList *l, void *data){
+    if(l != NULL){
+        SLNode *current;
+        current = l->first;
+        
+        SLNode *newnode = (SLNode *)malloc(sizeof(SLNode));
+
+
+        if(newnode != NULL){
+            newnode -> data = data;
+            newnode -> next = NULL;
+            if(current == NULL){
+                l->first = newnode;
+            }
+            else{
+                while(current->next != NULL){
+                    current = current->next;
+                }
+
+                current->next = newnode;
+            }
+
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+//para lista duplamente encadeada
+
+int dllInsertAsLast(DLList *l, void *data){
+    if(l != NULL){
+        DLNode *newnode = (DLNode *)malloc(sizeof(DLNode));
+
+        if(newnode != NULL){
+            newnode -> data = data;
+            newnode -> next = NULL;
+
+            DLNode *current = l->first;
+
+            if(current == NULL){
+                l->first = newnode;
+                newnode -> prev = NULL;
+            }
+            else{
+                while(current -> next != NULL){
+                    current = current -> next;
+                }
+
+                current -> next = newnode;
+                newnode -> prev = current;
+            }
+
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+
+//para lista circular simplesmente encadeada
+
+int csllInsertAsLast(CSLList *l, void *data){
+    if(l != NULL){
+        CSLNode *newnode = (CSLNode *)malloc(sizeof(CSLNode));
+        if(newnode != NULL){
+            newnode->data = data;
+            CSLNode *current = l->first;
+
+            if(current == NULL){
+                l->first = newnode;
+                newnode -> next = newnode;
+            }
+
+            else{
+                while(current->next != l->first){
+                    current = current -> next;
+                }
+
+                current -> next = newnode;
+                newnode -> next = l->first;
+            }
+
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+//para lista duplamente encadeada
+
+int cdllInsertAsLast(CDLList *l, void *data){
+    if(l != NULL){
+        CDLNode *newnode = (CDLNode *)malloc(sizeof(CDLNode));
+        if(newnode != NULL){
+            newnode -> data = data;
+            CDLNode *current = l->first;
+
+            if(current == NULL){
+                newnode -> next = newnode;
+                newnode -> prev = newnode;
+            }
+
+            else{
+                while(current -> next != l->first){
+                    current = current -> next;
+                }
+
+                l->first->prev = newnode;
+                current->next = newnode;
+                newnode -> prev = current;
+                newnode -> next = l->first;
+
+            }
+
+            return TRUE;
+        }
+
+    }
+
+    return FALSE;
+}
+
+//questão 1 letra c
+
+//para lista simplemente encadeada
+
+int sllInsertKPosition(SLList *l, void *data, int k){
+    if(l != NULL){
+        SLNode *newnode = (SLNode *)malloc(sizeof(SLNode));
+        if(newnode != NULL){
+            newnode -> data = data;
+
+            SLNode *current = l->first;
+            int i = 0;
+            while(current != NULL && i < k){
+                current = current -> next;
+                i += 1;
+            }
+
+            if(current != NULL){
+                newnode -> next = current -> next;
+                current -> next = newnode;
+                return TRUE;
+            }
+        }
+    }
+
+    return FALSE;
+}
+
